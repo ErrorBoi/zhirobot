681120f08d37225a4a966325c9303f35dc08b3dd
Mikhail Slabchenko <mikhail.slabchenko@gmail.com>
2020-04-19 01:38:02 +0300

    remove redundant code

--- bot/waiters.go
+++ bot/waiters.go
@@ -20,22 +20,14 @@ func (b *Bot) wakeUp() {
 }
 
 func (b *Bot) groupWeeklyNotification() {
-	ccfg := tgbotapi.ChatConfig{
-		ChatID: b.ChatID,
-	}
-	chat, err := b.BotAPI.GetChat(ccfg)
-	if err != nil {
-		b.lg.Errorf("Get chat error: %w", err)
-	}
-	text := tgbotapi.NewMessage(chat.ID, weeklyNotificationMessage)
-
+	text := tgbotapi.NewMessage(b.ChatID, weeklyNotificationMessage)
 	message, err := b.BotAPI.Send(text)
 	if err != nil {
 		b.lg.Errorf("Send message error: %w", err)
 	}
 
 	pcmcfg := tgbotapi.PinChatMessageConfig{
-		ChatID:              chat.ID,
+		ChatID:              b.ChatID,
 		MessageID:           message.MessageID,
 		DisableNotification: false,
 	}
@@ -58,8 +50,8 @@ func (b *Bot) usersWeeklyNotification() {
 		if err != nil {
 			b.lg.Errorf("Get chat error: %w", err)
 		}
-		text := tgbotapi.NewMessage(int64(user.TgID), fmt.Sprintf(weeklyUserNotificationMessage, chat.FirstName))
 
+		text := tgbotapi.NewMessage(int64(user.TgID), fmt.Sprintf(weeklyUserNotificationMessage, chat.FirstName))
 		_, err = b.BotAPI.Send(text)
 		if err != nil {
 			b.lg.Errorf("Send message error: %w", err)
